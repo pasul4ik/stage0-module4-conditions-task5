6ed121ab63445063760677b56b42c06fc468cff2
PavelDenisov <pavel.den111@gmail.com>
2023-02-22 11:45:31 +0200

    done

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        switch (year) {
+            case 1:
+                System.out.println(365);
+                break;
+
+            case 2 :
+                System.out.println(366);
+        }
+
+
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
@@ -2,5 +2,30 @@ package school.mjc.stage0.conditions.task5;
 
 public class RangeNumbersPrinter {
     public void printRange(int number) {
+
+        switch (number) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("number is between 1 and 5");
+                break;
+
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+                System.out.println("number is between 6 and 10");
+                break;
+            default:
+                System.out.println("out of range");
+
+
+        }
     }
 }
+//    Create that will be printing if number is between 1 and 5("number is between 1 and 5"),
+//        6 and 10("number is between 6 and 10") or "out of range" using switch statement.
+//        Number to be defined will be passed to method arguments:
